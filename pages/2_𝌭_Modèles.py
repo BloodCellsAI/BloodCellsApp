d9f1fb6672ccdd4b0a93a87bf6339d4aa433a271
Nicolas ORIEUX <nicolas.orieux@neuf.fr>
2023-06-18 20:36:27 +0200

    Nettoyage du dossier Images

--- pages/2_𝌭_Modèles.py
+++ pages/2_𝌭_Modèles.py
@@ -1,10 +1,60 @@
 import streamlit as st
 import pandas as pd
 from pathlib import Path
-
+from st_aggrid import AgGrid, GridOptionsBuilder, JsCode
 
 excel_path = Path("data/Tables/Models.xlsx")
-excel = pd.read_excel(excel_path, sheet_name="Models", header=0, keep_default_na=False, decimal=',')
+df = pd.read_excel(excel_path, sheet_name="Models", header=0, keep_default_na=False, decimal=',')
 
-st.dataframe(excel)
+gb = GridOptionsBuilder.from_dataframe(df)
+#gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+#gb.configure_side_bar() #Add a sidebar
+gb.configure_selection('single', use_checkbox=True) #Enable row selection
+gridOptions = gb.build()
+
+grid_response = AgGrid(
+    df,
+    gridOptions=gridOptions,
+    data_return_mode='AS_INPUT', 
+    update_mode='MODEL_CHANGED', 
+    fit_columns_on_grid_load=True,
+    enable_enterprise_modules=False,
+    height=350, 
+    width='100%',
+    reload_data=True
+)
+
+selected = grid_response['selected_rows'][0]
+
+#selected_row = AgGrid(df, gridOptions=gridOptions, height=300)
+
+st.text(selected)
+
+Modèle = selected['Modèle']
+Mask = selected['Mask']
+Augmentation = selected['Augmentation']
+Pretrain = selected['Pretrain']
+Fine_tuning = selected['Fine-tunning']
+Train = selected['Train']
+
+Filename = str(Modèle + '_' + Mask + '_' + Augmentation + '_')
+
+st.text(Filename)
+
+
+
+
+
+
+
+
+
+#AgGrid(excel)
+
+
+#st.dataframe(excel)
 #st.write(excel.to_html(), unsafe_allow_html=True)
+
+#selection = st.table(excel)
+
+
